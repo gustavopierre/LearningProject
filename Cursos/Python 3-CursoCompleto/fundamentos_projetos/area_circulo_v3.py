eb94bef9ae47c526dc1313ab9e82ccd774eec169
GustavoPierre <gustavopierre@gmail.com>
2020-02-05 17:49:44 -0300

    Melhorando a Mensagem de erro

--- Cursos/Python 3-CursoCompleto/fundamentos_projetos/area_circulo_v3.py
+++ Cursos/Python 3-CursoCompleto/fundamentos_projetos/area_circulo_v3.py
@@ -3,6 +3,11 @@ import sys
 import errno
 
 
+class TerminalColor:
+    ERRO = '\033[91m'
+    NORMAL = '\033[0m'
+
+
 def circulo(raio):
     return pi*(float(raio)**2)
 
@@ -19,9 +24,10 @@ if __name__ == '__main__':
 
     if not sys.argv[1].isnumeric():
         help()
-        print("O raio deve ser um valor numérico.")
+        print(TerminalColor.ERRO +
+              "O raio deve ser um valor numérico." + TerminalColor.NORMAL)
         sys.exit(errno.EINVAL)
-    
+
     raio = sys.argv[1]
     area = circulo(raio)
     print('A área do circulo é:', area)
