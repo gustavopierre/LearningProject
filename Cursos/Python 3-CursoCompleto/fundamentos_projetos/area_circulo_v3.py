bc163b33b47fa02c28ec1cf0319afc4ee19d7c18
GustavoPierre <gustavopierre@gmail.com>
2020-02-04 21:41:10 -0300

    Testando se o modulo e main e atualizando arquivo de planejamento

--- Cursos/Python 3-CursoCompleto/fundamentos_projetos/area_circulo_v3.py
+++ Cursos/Python 3-CursoCompleto/fundamentos_projetos/area_circulo_v3.py
@@ -1,9 +1,6 @@
 from math  import pi
 
-
-raio = input('Informe o raio: ')
-area = pi*(float(raio)**2)
-print(area)
-
-print('Nome do módulo: ', __name__)
-print('Nome do pacote: ', __package__)
+if  __name__ == '__main__':
+    raio = input('Informe o raio: ')
+    area = pi*(float(raio)**2)
+    print(area)
