0a2cd7f2398cfc83a4897d4399f549d147c324ed
GustavoPierre <gustavopierre@gmail.com>
2020-02-05 05:33:45 -0300

    Atualizando area_circulo_v3.py para exemplificar funçoes sem retorno

--- Cursos/Python 3-CursoCompleto/fundamentos_projetos/area_circulo_v3.py
+++ Cursos/Python 3-CursoCompleto/fundamentos_projetos/area_circulo_v3.py
@@ -1,6 +1,10 @@
 from math  import pi
 
-if  __name__ == '__main__':
-    raio = input('Informe o raio: ')
+def circulo(raio):
     area = pi*(float(raio)**2)
     print(area)
+
+if  __name__ == '__main__':
+    raio = input('Informe o raio: ')
+    circulo(raio)
+    
