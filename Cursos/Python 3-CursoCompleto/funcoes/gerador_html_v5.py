ea9a9dc003618718f1ba816a0428e294cd1d03e1
GustavoPierre <gustavopierre@gmail.com>
2020-02-11 21:44:30 -0300

    Continuando o capitulo de Funçoes

--- Cursos/Python 3-CursoCompleto/funcoes/gerador_html_v5.py
+++ Cursos/Python 3-CursoCompleto/funcoes/gerador_html_v5.py
@@ -1,13 +1,24 @@
 #!/usr/bin/python3
-def tag_bloco(conteudo, *args, classe='success', inline=False):
+bloco_atribs = ('bloco_accesskey', 'bloco_id')
+ul_atribs = ('ul_id', 'ul_style')
+
+
+def filtrar_atribs(informados, suportados):
+    return ' '.join(f'{k.split("_")[-1]}="{v}"'
+                    for k, v in informados.items() if k in suportados)
+
+
+def tag_bloco(conteudo, *args, classe='success', inline=False, **novos_atrbs):
     tag = 'span' if inline else 'div'
-    html = conteudo if not callable(conteudo) else conteudo(*args)
-    return f'<{tag} class="{classe}">{html}</{tag}>'
+    html = conteudo if not callable(conteudo) \
+        else conteudo(*args, **novos_atrbs)
+    atributos = filtrar_atribs(novos_atrbs, bloco_atribs)
+    return f'<{tag} {atributos} class="{classe}">{html}</{tag}>'
 
 
-def tag_lista(*itens):
+def tag_lista(*itens, **novos_atrbs):
     lista = ''.join(f'<li>{item}</li>' for item in itens)  # generator
-    return f'<ul>{lista}</ul>'
+    return f'<ul {filtrar_atribs(novos_atrbs, ul_atribs)}>{lista}</ul>'
 
 
 if __name__ == '__main__':
@@ -26,3 +37,5 @@ if __name__ == '__main__':
     # nomeados, não podem ser posicionais
     print(tag_bloco(tag_lista, 'Sábado', 'Domingo',
                     classe='info', inline=True))
+    print(tag_bloco(tag_lista, 'Item 1', 'Item 2', classe='info',
+                    bloco_accesskey='m', bloco_id='conteudo', ul_id='lista'))
