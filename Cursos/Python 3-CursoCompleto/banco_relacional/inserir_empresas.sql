9a23c1390d99564f4db7383b12351983061a5900
GustavoPierre <gustavopierre@gmail.com>
2020-02-28 20:10:16 -0300

    Modulo de Banco de Dados no Python iniciado

--- Cursos/Python 3-CursoCompleto/banco_relacional/inserir_empresas.sql
+++ Cursos/Python 3-CursoCompleto/banco_relacional/inserir_empresas.sql
@@ -7,3 +7,15 @@ VALUES
     ('Vale', 33821182000110),
     ('Cielo', 50782243000105);
 
+DESC empresas;
+desc prefeitos;
+select * from empresas;
+select * from cidades;
+
+insert into empresas_unidades
+    (empresa_id, cidade_id, sede)
+values 
+    (1, 1, 1),
+    (1, 2, 0),
+    (2, 1, 0),
+    (2, 2, 1);
